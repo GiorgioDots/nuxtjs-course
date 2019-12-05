40a56da19df646c86547eda9d67ff7bb98b01504
Giorgio Dodesini <giorgio.dodesini.it@gmail.com>
2019-12-06 00:55:30 +0100

    second module done

---
+++ Module2-PagesRoutingAndViews/module2-app/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <Header></Header>
+    <nuxt-link to="/users">Users</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ Module2-PagesRoutingAndViews/pages-views-assignment-problem/pages/products/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1>Products</h1>
+    <hr />
+    <nuxt-link to="/products/1">Go to product 1</nuxt-link>
+    <br />
+    <nuxt-link to="/products/2">Go to product 2</nuxt-link>
+  </div>
+</template>
