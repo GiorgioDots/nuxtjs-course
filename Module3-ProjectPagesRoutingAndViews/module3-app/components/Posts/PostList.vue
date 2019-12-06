f8b824b15dd2111c1574df9a69c700b0fb4d798a
Giorgio Dodesini <giorgio.dodesini.it@gmail.com>
2019-12-06 19:07:44 +0100

    module 3 done

---
+++ Module3-ProjectPagesRoutingAndViews/module3-app/components/Posts/PostList.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="post-list">
+    <PostPreview
+      id="1"
+      :is-admin="isAdmin"
+      thumbnail="https://static.techspot.com/images2/news/bigimage/2018/07/2018-07-10-image-35.jpg"
+      title="Hello There"
+      previewText="This is my first post!"
+    />
+    <PostPreview
+      id="2"
+      :is-admin="isAdmin"
+      thumbnail="https://static.techspot.com/images2/news/bigimage/2018/07/2018-07-10-image-35.jpg"
+      title="Hello There - the second time"
+      previewText="This is my second post!"
+    />
+    <PostPreview
+      id="3"
+      :is-admin="isAdmin"
+      thumbnail="https://static.techspot.com/images2/news/bigimage/2018/07/2018-07-10-image-35.jpg"
+      title="Hi"
+      previewText="This is my third post!"
+    />
+  </section>
+</template>
+<script>
+import PostPreview from "@/components/Posts/PostPreview";
+export default {
+  components: {
+    PostPreview
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
